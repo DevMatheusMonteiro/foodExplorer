2384949690efba5cb21580032a65ac5c578e89df
DevMatheusMonteiro <carlosmathmonteiro@gmail.com>
2024-03-18 18:11:06 -0300

    changed autocrlf to false

--- api/src/database/knex/migrations/20240318205933_createUsers.js
+++ api/src/database/knex/migrations/20240318205933_createUsers.js
@@ -1,16 +1,18 @@
 exports.up = (knex) =>
   knex.schema.createTable("users", (table) => {
     table.increments("id");
-    table.text("name");
-    table.text("email");
-    table.text("password");
+    table.text("name").notNullable();
+    table.text("email").notNullable();
+    table.text("password").notNullable();
+
     table
-      .enum("role", ["admin", "employee", "customer"], {
+      .enum("role", ["admin", "customer", "sale"], {
         useNative: true,
         enumName: "roles",
       })
       .notNullable()
       .default("customer");
+
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
   });
